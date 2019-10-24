22c230d8881cfe0722134827a50702397f77273e
Hee Sang Yoo <gmltkd90@gmail.com>
2019-10-23 17:49:51 -0700

    coins

--- coin.py
+++ coin.py
@@ -23,28 +23,34 @@ class Coin(Object):
             if self.frame is 1:
                 self.pic = pygame.image.load('images/blink_coin1.png')
                 self.frame = 2
+                self.image = pygame.transform.scale(self.pic, (self.settings.brick_lenth, self.settings.brick_lenth))
             elif self.frame is 2:
                 self.pic = pygame.image.load('images/blink_coin2.png')
                 self.frame = 3
+                self.image = pygame.transform.scale(self.pic, (self.settings.brick_lenth, self.settings.brick_lenth))
             elif self.frame is 3:
                 self.pic = pygame.image.load('images/blink_coin3.png')
                 self.frame = 1
+                self.image = pygame.transform.scale(self.pic, (self.settings.brick_lenth, self.settings.brick_lenth))
         elif self.otype is 2:
             if self.frame is 1:
                 self.pic = pygame.image.load('images/coin1.png')
                 self.frame = 2
+                self.image = pygame.transform.scale(self.pic, (self.settings.brick_lenth, self.settings.brick_lenth))
             elif self.frame is 2:
                 self.pic = pygame.image.load('images/coin2.png')
                 self.frame = 3
+                self.image = pygame.transform.scale(self.pic, (int(self.settings.spin_coin_width), self.settings.spin_coin_height))
             elif self.frame is 3:
                 self.pic = pygame.image.load('images/coin3.png')
                 self.frame = 1
+                self.image = pygame.transform.scale(self.pic, (int(self.settings.spin_coin_width), self.settings.brick_lenth))
 
         #if self.otype is 2:
         #    if self.frame is 2 or 3:
-        #        self.image = pygame.transform.scale(self.pic, (self.settings.spin_coin_width, self.settings.spin_coin_height))
+        #        self.image = pygame.transform.scale(self.pic, (int(self.settings.spin_coin_width), self.settings.spin_coin_height))
         #else:
-        self.image = pygame.transform.scale(self.pic, (self.settings.brick_lenth, self.settings.brick_lenth))
+        #    self.image = pygame.transform.scale(self.pic, (self.settings.brick_lenth, self.settings.brick_lenth))
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
